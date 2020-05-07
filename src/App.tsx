f6059afe52a8786cf81e36df7f0c87506af7d763
Santiago Palladino <spalladino@gmail.com>
2020-05-07 17:16:20 -0300

    Wire frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Relayer } from './Model';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listRelayers } from './graphql/queries';
+import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function App() {
+  const [relayers, setRelayers] = useState<Relayer[]>([]);
+  useEffect(() => { fetchRelayers() }, []);
+  const fetchRelayers = async () => {
+    const response = await API.graphql(graphqlOperation(listRelayers, { tenantId: "TENANT"})) as any; // sorrynotsorry
+    setRelayers(response.data.listRelayers.items as Relayer[]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Relayers</h1>
+      { relayers.map(relayer => (
+        <div key={relayer.id}>({relayer.id}) {relayer.name} of {relayer.tenantId}</div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/Model.ts
@@ -0,0 +1,5 @@
+export interface Relayer {
+  name: string;
+  id: string;
+  tenantId: string;
+}
